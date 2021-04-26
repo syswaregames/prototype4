e632a9e1f1cb024b5e4549c41cfe390048d79dc5
Sysware <sysware@sysware.com.br>
2021-04-26 01:08:14 -0300

    finish prototype 4

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -6,10 +6,11 @@ public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
     private float speed = 500;
-    private GameObject focalPoint;
+    private GameObject focalPoint;    
 
     public bool hasPowerup;
     public GameObject powerupIndicator;
+    public ParticleSystem somokeParticle;
     public int powerUpDuration = 5;
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
@@ -30,6 +31,12 @@ public class PlayerControllerX : MonoBehaviour
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
+        if (Input.GetKeyDown("space"))
+        {
+            somokeParticle.Play();
+            playerRb.AddForce(focalPoint.transform.forward * normalStrength, ForceMode.Impulse);
+        }
+
     }
 
     // If Player collides with powerup, activate powerup
@@ -40,6 +47,7 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
         }
     }
 
@@ -57,7 +65,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; //transform.position - other.gameObject.transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
